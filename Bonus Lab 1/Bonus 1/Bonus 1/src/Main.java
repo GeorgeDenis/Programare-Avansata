2e02cd405d3a8c16d3a33080e7a486cbd519befd
GeorgeDenisDuluta <36382081+GeorgeDenis@users.noreply.github.com>
2023-02-26 15:35:36 +0200

    Add files via upload

---
+++ Bonus Lab 1/Bonus 1/Bonus 1/src/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = validareNumar(args);
+        int [][] matrice = matriceAdiacenta(n);
+        int k=1;
+        System.out.println("A la puterea a-" + k +" a:");
+        afisareMatrice(matrice,n);
+        int [][] produs = inmultireMatrici(matrice,matrice,n);
+        k++;
+        System.out.println("A la puterea a-" + k +" a:");
+        afisareMatrice(produs,n);
+        for(int i=0;i<n-2;i++)
+        {
+
+            produs = inmultireMatrici(matrice,produs,n);
+            k++;
+            System.out.println("A la puterea a-" + k +" a:");
+            afisareMatrice(produs,n);
+        }
+
+    }
+    public static int validareNumar(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Nu ati introdus niciun numar natural");
+            System.exit(1);
+        }
+        int n = -1;
+        try {
+            n = Integer.parseInt(args[0]);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Argumentul introdus nu este un int " + args[0]);
+            System.exit(1);
+        }
+        return n;
+    }
+    public static int[][] matriceAdiacenta(int n) {
+        int[][] matrice = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            matrice[i][(i + 1) % n] = 1;
+            matrice[(i + 1) % n][i] = 1;
+        }
+        return matrice;
+    }
+    public static void afisareMatrice(int matrice[][],int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+                System.out.printf("%d ",matrice[i][j]);
+            System.out.println();
+        }
+    }
+    public static int[][] inmultireMatrici(int matrice1[][],int matrice2[][], int n)
+    {
+        int[][] produs = new int[n][n];
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+                for(int k = 0; k < n; k++)
+                    produs[i][j] = produs[i][j] + matrice1[i][k] * matrice2[k][j];
+        }
+        return produs;
+    }
+}
---
+++ Bonus Lab 1/Bonus 2/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int nrNoduri = Integer.parseInt(args[0]);
+        int grad = Integer.parseInt(args[1]);
+
+        if(grad >= nrNoduri)
+        {
+            System.out.println("Gradul nodurilor nu poate fi mai mare sau egal cu numarul de noduri");
+            System.exit(1);
+        }
+        if(grad % 2 != 0 && nrNoduri % 2 != 0)
+        {
+            System.out.println("Daca gradul nodurilor este un numar impar atunci numarul de noduri trebuie sa fie par");
+            System.exit(1);
+        }
+
+        int[][] matrice = creareMatrice(nrNoduri,grad);
+        afisareMatrice(matrice,nrNoduri);
+    }
+    public static void afisareMatrice(int[][] matrice,int n)
+
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+                System.out.printf("%d ",matrice[i][j]);
+            System.out.println();
+        }
+    }
+    public static int[][] creareMatrice(int nrNoduri, int grad) {
+        int[][] matrice = new int[nrNoduri][nrNoduri];
+        int muchii = nrNoduri * grad / 2;
+        int totalMuchii = 0;
+
+        for (int i = 0; i < nrNoduri; i++) {
+            for (int j = 1; j <= grad / 2; j++) {
+                int vecin = (i + j) % nrNoduri;
+                matrice[i][vecin] = 1;
+                matrice[vecin][i] = 1;
+                totalMuchii++;
+            }
+        }
+        if (grad % 2 == 1) {
+            for (int i = 0; i < nrNoduri && totalMuchii < muchii; i++) {
+                int vecin = (i + (nrNoduri / 2)) % nrNoduri;
+                matrice[i][vecin] = 1;
+                matrice[vecin][i] = 1;
+                totalMuchii++;
+            }
+        }
+        return matrice;
+    }
+
+
+
+}
